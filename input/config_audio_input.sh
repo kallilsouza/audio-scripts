fbebd2c4e0aa078997438213f67a5b4381b5d03a
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2024-04-03 23:52:39 -0300

    only sets volume if the environment variable VOLUME is set and is a number

--- input/config_audio_input.sh
+++ input/config_audio_input.sh
@@ -7,5 +7,11 @@ set +a
 
 pactl set-default-source "$DEFAULT_SOURCE"
 pactl load-module module-echo-cancel aec_method=webrtc sink_properties=device.description="Noise_Reduction" aec_args="analog_gain_control=0\ digital_gain_control=0"
-pactl set-default-source "${DEFAULT_SOURCE}.echo-cancel"
-pactl set-source-volume "${DEFAULT_SOURCE}.echo-cancel" 125%
+
+NEW_DEVICE_NAME="${DEFAULT_SOURCE}.echo-cancel"
+
+pactl set-default-source $NEW_DEVICE_NAME
+
+if [[ $VOLUME =~ ^[0-9]+$ ]]; then
+    pactl set-source-volume "${DEFAULT_SOURCE}.echo-cancel" $VOLUME%
+fi
