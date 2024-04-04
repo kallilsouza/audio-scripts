2337064db70f6d654a66d4fcf7443cf8873a92b9
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2024-04-03 23:20:57 -0300

    increasing input volume

--- input/config_audio_input.sh
+++ input/config_audio_input.sh
@@ -8,3 +8,4 @@ set +a
 pactl set-default-source "$DEFAULT_SOURCE"
 pactl load-module module-echo-cancel aec_method=webrtc sink_properties=device.description="Noise_Reduction" aec_args="analog_gain_control=0\ digital_gain_control=0"
 pactl set-default-source "${DEFAULT_SOURCE}.echo-cancel"
+pactl set-source-volume "${DEFAULT_SOURCE}.echo-cancel" 125%
