39ba3e23bc1c56e9fbb699d3f57d539b82693f37
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2024-08-29 23:14:05 -0300

    updating audio input

--- input/config_audio_input.sh
+++ input/config_audio_input.sh
@@ -8,7 +8,7 @@ set +a
 pactl set-default-source "$DEFAULT_SOURCE"
 pactl load-module module-echo-cancel aec_method=webrtc sink_properties=device.description="Noise_Reduction" aec_args="analog_gain_control=0\ digital_gain_control=0"
 
-NEW_DEVICE_NAME="${DEFAULT_SOURCE}.echo-cancel"
+NEW_DEVICE_NAME="echo-cancel-source"
 
 pactl set-default-source $NEW_DEVICE_NAME
 
